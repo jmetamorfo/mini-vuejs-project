48a497c898516b5f654fa6ca83e2d38089df2a81
Juan Munoz <juan.munoz@lemon42.com>
2017-05-21 13:29:16 -0700

    Add header,content,footer, etc components to the project

---
+++ src/ServerContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <app-servers></app-servers>
+      <app-server-detail></app-server-detail>
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+
+</template>
+
+<script>
+  import Footer from './Footer.vue'
+  import Header from './Header.vue'
+  import Servers from './Servers.vue'
+  import ServerDetail from './ServerDetail.vue'
+  
+  export default {
+      components: {
+        'app-footer': Footer,
+        'app-header': Header,
+        'app-servers': Servers,
+        'app-server-detail': ServerDetail
+      }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ServerContent from './ServerContent.vue'
+
+Vue.component('app-server-content', ServerContent);
 
 new Vue({
   el: '#app',
